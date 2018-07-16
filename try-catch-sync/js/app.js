a047158715f5633ead056612dd4a2a50ddc00c93
memecast <amecgavante1305@gmail.com>
2018-07-16 10:13:14 -0500

    declaracion de una excepcion

---
+++ try-catch-sync/js/app.js
@@ -0,0 +1,22 @@
+'use strict';
+// Declaracion de una excepcion
+
+function userExc (mensaje){
+    this.mensaje = mensaje;
+    this.nombre = "userException";
+}
+
+userExc.prototype.toString = function (){
+    return `${this.nombre} : "${this.mensaje}"`
+};
+
+// let 1234w excepcion.toString();
+let excepcion =  new userExc('Todo Mal');
+throw excepcion.toString();
+
+let numero = prompt("ingresa 5 numeros");
+
+if(numero.length < 5){
+    let excepcion =  new userExc('El numero ingresado no tiene la longitud necesaria');
+    throw excepcion.toString();
+}
