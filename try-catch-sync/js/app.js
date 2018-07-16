7783cd962f6b39b003d6633250486f978db9d362
memecast <amecgavante1305@gmail.com>
2018-07-16 11:41:28 -0500

    ejercicio try y catch nombre

--- try-catch-sync/js/app.js
+++ try-catch-sync/js/app.js
@@ -15,9 +15,33 @@ userExc.prototype.toString = function (){
 
 // Para qué nos serviria declarar una excepción
 
-let numero = prompt("ingresa 5 numeros");
+// let numero = prompt("ingresa 5 numeros");
 
-if(numero.length < 5){
-    let excepcion =  new userExc('El numero ingresado no tiene la longitud necesaria');
-    throw excepcion.toString();
-}
+// if(numero.length < 5){
+//     let excepcion =  new userExc('El numero ingresado no tiene la longitud necesaria');
+//     throw excepcion.toString();
+// }
+
+// TRY CATCH 
+
+/*
+Declarar un bloque try-catch indica que nuestro codigo puede o no ejecutarse, puede o no pasar
+algo que dañe la ejecucion de nuestro codigo y puede que necesitamos saber exactamente que ocurrio
+    1. TRY: Busca ejecutar el codigo sin problemas, 
+    si un problema ocurre entonces se atrapan los errores arrojados (throw)
+    2. CATCH: El bloque catch solo se ejecuta al recibir un error arrojado. 
+    3. finally: Ejecuta las lineas de codigo que resultan despues del bloque try. 
+*/
+let nombre = prompt("ingresa un nombre de minimo 6 letras");
+try{
+
+    if(nombre.length < 6){
+        let excepcion =  new userExc('El nombre ingresado no tiene la longitud necesaria');
+        nombre = "nombre"
+        throw excepcion.toString();
+    }
+}catch(e){
+    console.log(e);
+}finally{
+    alert(`El nombre ingresado es ${nombre}`);
+};
